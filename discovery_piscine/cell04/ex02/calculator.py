d7c2046910ad8903b7acf46e4fe22e6aaa9d980f
Wanwisa179 <66070179@kmitl.ac.th>
2026-09-21 16:47:49 +0700

    cell4.2

---
+++ discovery_piscine/cell04/ex02/calculator.py
@@ -0,0 +1,6 @@
+num1 = int(input("Give me the first number: "))
+num2 = int(input("Give me the second number: "))
+print(str(num1)+" + "+str(num2)+" = "+str(num1+num2))
+print(str(num1)+" - "+str(num2)+" = "+str(num1-num2))
+print(str(num1)+" / "+str(num2)+" = "+str(int(num1/num2)))
+print(str(num1)+" * "+str(num2)+" = "+str(num1*num2))
