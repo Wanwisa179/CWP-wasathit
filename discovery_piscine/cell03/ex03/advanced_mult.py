969528ec5ad6ea615b956fc48c34977238eb5052
Wanwisa179 <66070179@kmitl.ac.th>
2026-09-22 17:11:15 +0700

    cell06

--- discovery_piscine/cell03/ex03/advanced_mult.py
+++ discovery_piscine/cell03/ex03/advanced_mult.py
@@ -1,12 +1,17 @@
+import sys
 loop = 0
 roop = 0
-while loop < 11:
-    print("Table de "+str(loop)+": ", end="")
-    while roop < 11:
-        if(roop == 10):
-            print(str(loop*roop)+" ")
-        else:
-            print(str(loop*roop)+" ", end="")
-        roop = roop+1
-    roop = 0
-    loop = loop+1
+
+if len(sys.argv) > 1:
+    print("none")
+else:
+    while loop < 11:
+        print("Table de "+str(loop)+": ", end="")
+        while roop < 11:
+            if(roop == 10):
+                print(str(loop*roop)+" ")
+            else:
+                print(str(loop*roop)+" ", end="")
+            roop = roop+1
+        roop = 0
+        loop = loop+1
